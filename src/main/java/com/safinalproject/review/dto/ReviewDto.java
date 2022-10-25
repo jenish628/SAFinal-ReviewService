0660aff59de5799e3af901a7e30d253146fec426
Sandeep <sndpkrl007@gmail.com>
2022-10-24 23:05:16 -0500

    Redis config

--- src/main/java/com/safinalproject/review/dto/ReviewDto.java
+++ src/main/java/com/safinalproject/review/dto/ReviewDto.java
@@ -15,7 +15,6 @@ public class ReviewDto implements Serializable {
     private Long customerId;
     private String user;
     private Long vin;
-//    @Size(min = 1, max = 10, message = "Between the range 1-10")
     @Max(value = 10, message = "Maximum limit exceed {value}")
     @Min(value = 1, message = "Minimum limit {value} must be fulfilled")
     private int rating;
